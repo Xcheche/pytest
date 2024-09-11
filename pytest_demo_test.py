ab80d2612b0541ec42ab7f0b6dd8927c96dfc3f6
xcheche <chekwubechukwu23@gmail.com>
2024-09-11 21:03:23 +0100

    done with unittest--now pytest

---
+++ pytest_demo_test.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+def test_methodA():
+    print("test_methodA Execution...")
+
+
+def test_methodB():
+    print("test_methodB Execution...")
+
+
+def test_methodAC():
+    print("test_methodC Execution...")
